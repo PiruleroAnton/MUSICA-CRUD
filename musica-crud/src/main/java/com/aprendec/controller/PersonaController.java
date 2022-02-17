43b2d1c8b5f3e3a24d4027c0baf8125bd76f7181
PiruleroAnton <piruleroanton2017@gmail.com>
2022-02-17 11:25:10 +0100

    Primera version funcional

--- musica-crud/src/main/java/com/aprendec/controller/PersonaController.java
+++ musica-crud/src/main/java/com/aprendec/controller/PersonaController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.aprendec.model.Persona;
+import com.aprendec.model.musico_pruebas;
 import com.aprendec.service.PersonaService;
 import com.aprendec.service.MusicoService; 
 
@@ -27,11 +28,7 @@ public class PersonaController {
 		return "index";
 	}
 	
-	@RequestMapping("/index2")
-	public String index2(Model model) {
-		model.addAttribute("list", musicoService.getAll());
-		return "index2";
-	}
+	
 
 	@GetMapping("/save/{id}")
 	public String showSave(@PathVariable("id") Long id, Model model) {
@@ -54,6 +51,33 @@ public class PersonaController {
 		personaService.delete(id);
 
 		return "redirect:/";
+	}
+	
+	//mapeos para la nueva pagina de musicos
+	@RequestMapping("/index2")
+	public String index2(Model model) {
+		model.addAttribute("list", musicoService.getAll());
+		return "index2";
+	}
+	@GetMapping("/save2/{id}")
+	public String showSave2(@PathVariable("id") Long id, Model model) {
+		if (id != null && id != 0) {
+			model.addAttribute("musico", musicoService.get(id));
+		} else {
+			model.addAttribute("musico", new musico_pruebas());
+		}
+		return "save2";
+	}
+	@PostMapping("/save2")
+	public String save2(musico_pruebas musico, Model model) {
+		musicoService.save(musico);
+		return "redirect:/index2";
+	}
+	@GetMapping("/delete2/{id}")
+	public String delete2(@PathVariable Long id, Model model) {
+		musicoService.delete(id);
+
+		return "redirect:/index2";
 	}
 
 }
